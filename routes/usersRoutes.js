8d08b46c956c0059ef9ab9ee63bc74e831167088
Gabriel <gabriel.desimone@mail.com>
2018-03-25 12:24:17 -0300

    Last Changes to mock back

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -19,11 +19,36 @@ router.get('/info', authCheck, (req, res) => {
   res.json({ name:  'Administrator', permisions: ['admin']});
 });
 
-//search users (it doesnt go here!!)
+//search guest (it doesnt go here!!)
 router.post('/search', authCheck, (req, res) => {
-  res.json([{name: 'Gabriel', surname: 'Desimone', age: 26, local: 'Malvinas'},
-{name: 'Cintia', surname: 'Godoy', age: 26, local: 'JCP'}]);
+  res.json([{id: '001', name: 'Gabriel', surname: 'Desimone', age: 26, local: 'Malvinas'},
+{id: '002', name: 'Cintia', surname: 'Godoy', age: 26, local: 'JCP'}]);
 });
 
+//get a Guest Info
+router.get('/info/:guestId', authCheck, (req, res) => {
+  const {guestId} = req.params;
+  if (guestId === "001") {
+    res.json({id: guestId, name: 'Gabriel', surname: 'Desimone', dayOne: true, dayTwo: false, dayThree: false, contribution: true});
+  }
+  else {
+    res.status(403);
+    res.json({msg: 'Error'});
+  }
+});
+
+//subscribe a guest
+router.post('/guest', authCheck, (req, res) => {
+  const body = req.body;
+  console.log(JSON.stringify(body));
+  res.status(200).json({msg: 'invitado guardado'});
+});
+
+//update a guest
+router.put('/guest', authCheck, (req, res) => {
+  const body = req.body;
+  console.log(JSON.stringify(body));
+  res.status(200).json({msg: 'invitado actualizado'});
+});
 
 module.exports = router;
--- utils/security.js
+++ utils/security.js
@@ -4,9 +4,13 @@ const authCheck = (req, res, next) => {
   if (req.headers.token) {
     const token = req.headers.token;
     if (token === 'VALID') next();
-    res.status(401).json({ msg: 'Invalid Token!'});
+    else {
+    	res.status(401).json({ msg: 'Invalid Token!'});	
+    }
   }
-  res.status(401).json({ msg: 'Non-provided Token!'});
+  else {
+  	res.status(401).json({ msg: 'Non-provided Token!'});
+  }
 }
 
 module.exports = { authCheck };
